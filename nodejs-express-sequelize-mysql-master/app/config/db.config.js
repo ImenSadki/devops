f8eb438c7b2aaa7a7884cdb1cc8b57f1ce15121a
imen sadki <imensadki99@example.com>
2025-01-05 21:11:48 +0100

    add dockercompose

--- nodejs-express-sequelize-mysql-master/app/config/db.config.js
+++ nodejs-express-sequelize-mysql-master/app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "localhost",
+  HOST: "db",  // Changez localhost par db, car c'est le nom du service de la base de données dans Docker
   USER: "root",
   PASSWORD: "0000",  // Votre mot de passe MySQL
   DB: "projet_dev",
